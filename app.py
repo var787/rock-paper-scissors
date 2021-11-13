c8128bc31c8d75f9e787b04e4e65ae8f1649dd35
Rakshan Hegde <69235121+var787@users.noreply.github.com>
2021-11-13 17:03:27 -0600

    completed

--- app.py
+++ app.py
@@ -1,2 +1,27 @@
 # Play Rock Paper Scissor
 
+import random
+
+
+def play():
+    user = input("What's Your Choice?\nPick 'r' for Rock, 'p' for Paper and 's' for Scissor ")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return 'It\'s a Tie'
+
+    if won(user, computer):
+        return 'You Won!'
+
+    return 'You Lost :('
+
+
+
+# r>s, s>p, p>r
+def won(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+            or (player == 'p' and opponent == 'r'):
+        return True
+
+
+print(play())
